6891f84f0bb3d1a6568ed1894c3f0030b99262c6
DmitryLiashenko <dmliash1@gmail.com>
2024-01-25 20:11:55 +0200

    Commit-ready-hw-3-1

--- goit-algo-hw-03-1.py
+++ goit-algo-hw-03-1.py
@@ -1,2 +1,12 @@
-import math, random, datetime
-from datetime import datetime
+                        #  ФУНКЦИЯ ВОЗВРАЩАЕТ КОЛ-ВО ДНЕЙ ОТ ВВЕДЕННОЙ ДАТЫ К СЕГОДНЯШНЕЙ
+
+from datetime import datetime                                           # ИМПОРТИРУЕМ datetime из datetime
+def get_days_from_today():                                              # СОЗДАЕМ ФУНКЦИЮ
+    try:                                                                # НАЧАЛО БЛОКА ОБРАБОТКИ ОШИБОК
+        user_input = input("Введите дату в формате ГГГГ-ММ-ДД : ")      # ВВЕДЕНИЕ ДАТЫ ПОЛЬЗОВАТЕЛЕМ
+        user_data = datetime.strptime(user_input, '%Y-%m-%d')           # ПРИВЕДЕНИЕ В ОБЪЕКТ datetime 
+        curent_date = datetime.now()                                    # ПОЛУЧАЕМ СЕГОДНЯШНЮЮ ДАТУ
+        quantity_days = curent_date - user_data                         # ВЫЧИСЛЯЕМ РАЗНИЦУ МЕЖДУ ВВЕДЕННОЙ И СЕГОДНЯШНЕЙ ДАТОЙ    
+        return quantity_days.days                                       # ВОЗВРАЩАЕМ В ФУНКЦИЮ РАЗНИЦУ ТОЛЬКО В ДНЯХ  
+    except Exception:                                                   # КОНЕЦ БЛОКА ОБРАБОТКИ ОШИБОК НЕВЕРНОГО ВВОДА ДАННЫХ
+        print("Дата введена в неверном формате или даты не существует") # СООБЩАЕМ ОБ ОШИБКЕ
