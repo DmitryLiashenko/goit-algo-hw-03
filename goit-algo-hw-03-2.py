ca33c8b7c658294f3ed6ea45497e151ba56f326f
DmitryLiashenko <dmliash1@gmail.com>
2024-01-25 23:58:11 +0200

    Commit-ready-hw-3-2

--- goit-algo-hw-03-2.py
+++ goit-algo-hw-03-2.py
@@ -1,17 +1,14 @@
                             #  Функция для генерирования случайных чисел для лотереи
 import random
-def get_numbers_ticket(min, max, quantity):                       # СОЗДАЕМ ФУНКЦИЮ
-    if min >= 1 and max <=1000 and quantity <= max:               # ПРОВЕРЯЕМ ПАРАМЕТРЫ 
-        list_with_lotery_digits = []                              # СОЗДАЕМ ПУСТОЙ СПИСОК
-        while quantity >= 1:                                      # СОЗДАЕМ ЦИКЛ                                          
-            random_digit = random.randint(min, max) 
-            for i in list_with_lotery_digits: 
-                if random_digit != i:                             #
-                    list_with_lotery_digits.append(random_digit)  #
-                quantity = quantity - 1            
-    else:
-        print("Числа выходят из заданного диапазон\nМинимальное значение 1, максимальное 1000\nКолличество желаемых чисел не больше 1000 ")   #
-    return sorted(list_with_lotery_digits)                       #
-                   
-print(get_numbers_ticket(1, 100, 5))
-
+def get_numbers_ticket(min, max, quantity):                   # СОЗДАЕМ ФУНКЦИЮ      
+    if min >= 1 and max <=1000 and quantity <= max:           # ПРОВЕРЯЕМ СООТВЕТСВУЮТ ЛИ ПАРАМЕТРЫ           
+        list_with_lotery_digits = []                          # СОЗДАЕМ ПУСТОЙ СПИСОК           
+        while quantity >= 1:                                  # ЗАПУСКАЕМ ЦИКЛ С НУЖНЫМ КОЛЛИЧЕСТВОМ ПОВТОРЕНИЙ                                  
+            random_digit = random.randint(min, max)           # ПОЛУЧАЕМ СЛУЧАЙНОЕ ЧИСЛО ИЗ ЗАДАНООГО ДИАПАЗОНА
+            if random_digit not in list_with_lotery_digits:   # ПРОВЕРЯЕМ ЧТО БЫ ЭТО ЧИСЛО УЖЕ НЕ БЫЛО В СПИСКЕ                        
+                list_with_lotery_digits.append(random_digit)  # ДОБАВЛЯЕМ ЧИСЛО В СПИСОК           
+                quantity = quantity - 1                       # УМЕНШАЕМ ИТЕРАЦИЮ ЦИКЛА НА 1 (если число уже есть в списке,то итерация не уменшается)
+        return sorted(list_with_lotery_digits)                # СОРТИРУЕМ СПИСОК И ВОЗВРАЩАЕМ В ФУНКЦИЮ   
+    else:                                                     # ВЫВОДИМ ПРЕДУПРЕЖДЕНИЕ ЕСЛИ ЗАДАННЫЕ АРГУМЕНТЫ НЕ СООТВЕТСВУЮТ ДИАПАЗОНУ
+        print("Числа выходят из заданного диапазон\nМинимальное значение 1, максимальное 1000\nКолличество желаемых чисел не больше 1000 ")   
+print(get_numbers_ticket(1, 100, 6))
